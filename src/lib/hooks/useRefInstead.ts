56cac8ade72e887bf2875c48edb3074aa9c7b391
Andrii Volynets <volinec449@gmail.com>
2024-09-01 13:03:45 +0300

    Remove: Removed unused declarations

--- src/lib/hooks/useRefInstead.ts
+++
@@ -1,9 +0,0 @@
-import { RefObject, useRef } from "react";
-
-const useRefInstead = <T>(ref?: RefObject<T>): RefObject<T> => {
-  const internalRef = useRef<T>(null);
-
-  return ref ?? internalRef;
-};
-
-export default useRefInstead;
--- src/lib/utils/schedulers.ts
+++ src/lib/utils/schedulers.ts
@@ -23,36 +23,3 @@ export function debounce<F extends AnyToVoidFunction>(
     }, ms);
   };
 }
-
-export function throttle<F extends AnyToVoidFunction>(
-  fn: F,
-  ms: number,
-  shouldRunFirst = true,
-) {
-  let interval: number | undefined;
-  let isPending: boolean;
-  let args: Parameters<F>;
-
-  return (..._args: Parameters<F>) => {
-    isPending = true;
-    args = _args;
-
-    if (!interval) {
-      if (shouldRunFirst) {
-        isPending = false;
-        fn(...args);
-      }
-
-      interval = self.setInterval(() => {
-        if (!isPending) {
-          self.clearInterval(interval!);
-          interval = undefined;
-          return;
-        }
-
-        isPending = false;
-        fn(...args);
-      }, ms);
-    }
-  };
-}
